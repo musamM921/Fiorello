14c27eb50a19e989c1c1e93b18dad4b33ce215f0
musamM921 <135432572+musamM921@users.noreply.github.com>
2023-06-21 22:38:41 +0400

    fiorello

---
+++ fiorello/assets/css/js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+
+    // HEADER
+
+    $(document).on('click', '#search', function () {
+        $(this).next().toggle();
+    })
+
+    $(document).on('click', '#mobile-navbar-close', function () {
+        $(this).parent().removeClass("active");
+
+    })
+    $(document).on('click', '#mobile-navbar-show', function () {
+        $('.mobile-navbar').addClass("active");
+
+    })
+
+    $(document).on('click', '.mobile-navbar ul li a', function () {
+        if ($(this).children('i').hasClass('fa-caret-right')) {
+            $(this).children('i').removeClass('fa-caret-right').addClass('fa-sort-down')
+        }
+        else {
+            $(this).children('i').removeClass('fa-sort-down').addClass('fa-caret-right')
+        }
+        $(this).parent().next().slideToggle();
+    })
+
+    // SLIDER
+
+    $(document).ready(function () {
+        $(".slider").owlCarousel(
+            {
+                items: 1,
+                loop: true,
+                autoplay: true
+            }
+        );
+    });
+
+});
+
